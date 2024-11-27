618473bf85ce3c5d6b87abb0263912723a16c234
Shalini Roy <114976861+shalr-20@users.noreply.github.com>
2024-11-27 23:30:40 +0530

    Redirecting on activity_main2 xml

--- FirstFrag.java
+++ FirstFrag.java
@@ -1,5 +1,6 @@
 package com.tweak.fragments.frag;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,7 +8,9 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import com.tweak.fragments.MainActivity2;
 import com.tweak.fragments.R;
 
 public class FirstFrag extends Fragment {
@@ -16,12 +19,22 @@ public class FirstFrag extends Fragment {
         // Required empty public constructor
     }
 
+    TextView tv;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view= inflater.inflate(R.layout.fragment_first, container, false);
 
+        tv=view.findViewById(R.id.textview);
+        tv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(getContext(), MainActivity2.class);
+                startActivity(intent);
+            }
+        });
 
 
 
